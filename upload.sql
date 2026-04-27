e60fcc1f2579675f27a7f24be782e98e634fc4f5
Patrick Lynch <patrick.r.lynch69@gmail.com>
2026-04-27 06:08:01 -0700

    Update upload.sql

--- upload.sql
+++ upload.sql
@@ -1,2 +1,2 @@
-copy "Batting" from '/path/to/Batting.csv' with csv header;
-copy "Batting" from '/path/to/BattingPost.csv' with csv header;
+copy "batting" from '/path/to/Batting.csv' with csv header;
+copy "battingpost" from '/path/to/BattingPost.csv' with csv header;
